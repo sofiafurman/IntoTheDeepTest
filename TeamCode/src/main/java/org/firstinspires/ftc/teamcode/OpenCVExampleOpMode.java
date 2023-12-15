a4966b40561795a27a49277439ec456aa62e4029
klaas75 <klaas75@libertycommon.org>
2023-12-14 20:43:51 -0700

    got the opencv up and working well, detects red and working towards detecting from specific regions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVExampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVExampleOpMode.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -55,38 +58,59 @@ public class OpenCVExampleOpMode extends OpMode {
 
 class SamplePipeline extends OpenCvPipeline {
 
-    Mat YCrCb = new Mat();
-    Mat Y = new Mat();
+    //Mat Binary = new Mat();
+    Mat HSV = new Mat();
+    Mat hsv1 = new Mat();
+    Mat hsv2 = new Mat();
     int avg;
+    Scalar lowerBound;
+    Scalar upperBound;
 
 
     /*
-     * This function takes the RGB frame, converts to YCrCb,
-     * and extracts the Y channel to the 'Y' variable
+     * This function takes the RGB frame, converts to HSV,
+     * and extracts the red color to the '' variable
      */
-    void inputToY(Mat input) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        ArrayList<Mat> yCrCbChannels = new ArrayList<Mat>(3);
-        Core.split(YCrCb, yCrCbChannels);
-        Y = yCrCbChannels.get(0);
+    void inputToRedBinary(Mat input) {
+        Imgproc.cvtColor(input, HSV, Imgproc.COLOR_RGB2HSV);
+        // get red pixels (hue > 0)
+        lowerBound = new Scalar(0, 200, 30);
+        upperBound = new Scalar(5, 255, 255);
+        Core.inRange(HSV, lowerBound, upperBound, hsv1);
+        // get red pixels (hue < 180)
+        lowerBound = new Scalar(170, 200, 30);
+        upperBound = new Scalar(180, 255, 255);
+        Core.inRange(HSV, lowerBound, upperBound, hsv2);
 
+        Core.add(hsv1, hsv2, HSV);
     }
 
     @Override
     public void init(Mat firstFrame) {
-        inputToY(firstFrame);
+        inputToRedBinary(firstFrame);
     }
 
     @Override
     public Mat processFrame(Mat input) {
-        inputToY(input);
+        inputToRedBinary(input);
         System.out.println("processing requested");
-        avg = (int) Core.mean(Y).val[0];
-        YCrCb.release(); // don't leak memory!
-        Y.release(); // don't leak memory!
-        return input;
-    }
+        //avg = (int) Core.mean(HSV).val[0];
+        //Point pt = new Point(50,100);
+        Scalar col = new Scalar(255);
+        //Imgproc.putText(HSV, avg + "", pt, 1, 10, col, 5);
+        Rect lrect = new Rect(120, 250, 300, 300);
+        Imgproc.rectangle(HSV, lrect, col, 5);
+        Rect mrect = new Rect(900, 290, 250, 250);
+        Imgproc.rectangle(HSV, mrect, col, 5);
+        Rect rrect = new Rect(1650, 250, 300, 300);
+        Imgproc.rectangle(HSV, rrect, col, 5);
 
+        //HSV.release(); // don't leak memory! // lol nvm we leakin fs
+        hsv1.release(); // don't leak memory!
+        hsv2.release(); // don't leak memory!
+        //Binary.release();
+        return HSV;
+    }
     public int getAnalysis() {
         return avg;
     }
